d74a9a920289835d210f3d7859e97157d23e7101
GonzaSotelo <g.sotelo.dab@gmail.com>
2024-07-30 16:00:26 -0300

    home

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const textElement = document.querySelector(".typing");
+    const text = "Estas a un paso de hacer tu sueño realidad.. ";
+    let index = 0;
 
+    function type() {
+        if (index < text.length) {
+            textElement.textContent += text.charAt(index);
+            index++;
+            setTimeout(type, 100); // Ajusta la velocidad de tipeo aquí (en milisegundos)
+        }
+    }
+
+    type();
+});
 
 
 document.querySelector('.header__container_icon-menu').addEventListener('click', function() {
